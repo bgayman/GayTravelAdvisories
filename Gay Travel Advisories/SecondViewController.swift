b023827d10dec03b5eaeeee2b5796f8444a2474a
bgayman <bgayman@mac.com>
2017-09-27 20:43:01 -0400

    Initial Commit

---
+++ Gay Travel Advisories/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gay Travel Advisories
+//
+//  Created by Brad G. on 9/27/17.
+//  Copyright © 2017 Brad G. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
